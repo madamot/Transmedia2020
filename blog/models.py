605f2a75393126f9cc2308812aadf4fa1a214caa
Madamot <adam@madamot.com>
2020-01-26 21:19:40 +0000

    blog model added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class blog(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE,)
+    body = models.TextField()
+    date = models.DateField(("Date"), default=datetime.date.today)
+    blogImg = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title
