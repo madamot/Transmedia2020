da7e37843c7c9b14d82f3381e0763b6dc367d450
Madamot <adam@madamot.com>
2020-01-27 23:40:32 +0000

    blog list urls and views and templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from django.views.generic import ListView
+
+from .models import blog
+
 # Create your views here.
+class BlogListView(ListView):
+        model = blog
+        template_name = 'blog.html'
